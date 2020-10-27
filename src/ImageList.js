f328d801a797c4b341fe3028959676f2a93072f8
davidabutler92 <davidabutler92@yahoo.com>
2020-10-27 15:52:48 -0700

    add selection option with filters

--- src/ImageList.js
+++ src/ImageList.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 import ImageItem from './ImageItem.js';
-import images from './data.js';
 
 export default class ImageList extends Component {
+    state = {
+        filter: ''
+    }
+
+    clickHandler = e => {
+        this.setState ({
+            filter: e.target.value
+        })
+    }
+
     render() {
+        const filteredImages = this.props.images.filter((image) => {
+            if(!this.state.filter) return true; 
+
+            if(image.horns === Number(this.state.filter)) return true 
+            
+            return false 
+        });
+
         return (
+            <>
+            <div className="select">
+                <select onChange={this.clickHandler}>
+                    <option value=''>Show All</option>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='100'>100</option>
+                </select>
+            </div>
             <div className="articles">
             { 
-                images.map(image => 
+                filteredImages.map(image => 
                 <ImageItem 
                 url={image.url}
                 title={image.title}
@@ -15,6 +42,7 @@ export default class ImageList extends Component {
                 /> )
             }
             </div>
+            </>
         )
     }
 }
