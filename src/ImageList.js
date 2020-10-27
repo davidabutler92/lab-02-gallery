9daa18c5928a4e48b643be28f50b89756858aeaa
davidabutler92 <davidabutler92@yahoo.com>
2020-10-27 16:53:20 -0700

    add filter by keyword

--- src/ImageList.js
+++ src/ImageList.js
@@ -17,6 +17,8 @@ export default class ImageList extends Component {
             if(!this.state.filter) return true; 
 
             if(image.horns === Number(this.state.filter)) return true 
+
+            if(image.keyword === this.state.filter) return true
             
             return false 
         });
@@ -32,7 +34,22 @@ export default class ImageList extends Component {
                     <option value='100'>100</option>
                 </select>
             </div>
-            <div className="articles">
+            <div className="select">
+                <select onChange={this.clickHandler}>
+                    <option value=''>Show All</option>
+                    <option value='narwhal'>Narwhal</option>
+                    <option value='rhino'>Rhino</option>
+                    <option value='unicorn'>Unicorn</option>
+                    <option value='unilego'>Unilego</option>
+                    <option value='triceratops'>Triceratops</option>
+                    <option value='markhor'>Markhor</option>
+                    <option value='mouflon'>Mouflon</option>
+                    <option value='chameleon'>Chameleon</option>
+                    <option value='lizard'>Lizard</option>
+                    <option value='dragon'>Dragon</option>
+                </select>
+            </div>
+            <div className="articles section">
             { 
                 filteredImages.map(image => 
                 <ImageItem 
